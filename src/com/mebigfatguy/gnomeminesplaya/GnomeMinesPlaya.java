63456d073752ae32fb4083a5c4814ed345d2c74d
Dave Brosius <dbrosius@mebigfatguy.com>
2019-02-23 23:22:31 -0500

    Happy New Year

--- src/com/mebigfatguy/gnomeminesplaya/GnomeMinesPlaya.java
+++ src/com/mebigfatguy/gnomeminesplaya/GnomeMinesPlaya.java
@@ -1,7 +1,7 @@
 /*
  * gnomeminesplaya - An app that plays the gnome mines game
- * Copyright 2011-2013 MeBigFatGuy.com
- * Copyright 2011-2013 Dave Brosius
+ * Copyright 2011-2019 MeBigFatGuy.com
+ * Copyright 2011-2019 Dave Brosius
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
